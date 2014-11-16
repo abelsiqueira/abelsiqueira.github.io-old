fec826e24a70a722a00a8fdb4f6ed8e11092c4cc
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-11-16 01:46:02 -0200

    Add new post creator

---
+++ newpost.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cats=($(grep "^  [a-z]" _config.yml | sed 's/  \(.*\):/\1/g'))
+N=$((${#cats}-1))
+
+r=0
+while [ $r -lt 1 -o $r -gt $N ]; do
+  echo "Choose a category for this new post:"
+  for i in $(seq 0 $(($N-1)))
+  do
+    echo $(($i+1)) ${cats[$i]}
+  done
+  read r
+  if [ $r -lt 1 -o $r -gt $N ]; then
+    echo "Choose a number between 1 and $N"
+  fi
+done
+
+echo -n "Post title: "
+read title
+
+filename=$(date +"%Y-%m-%d")-$(echo $title | tr '[:upper:] ' '[:lower:]-')
+
+cat >> _posts/$filename << EOF
+---
+layout:     post
+title:      $title
+date:       $(date +"%Y-%m-%d")
+categories: ${cats[$(($r-1))]}
+---
+<p>
+</p>
+EOF
