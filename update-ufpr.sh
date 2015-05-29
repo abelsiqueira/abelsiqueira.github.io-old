12027d94ef37677b99d41abff188a4f7636b7dd6
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2015-05-29 14:53:03 -0300

    Adiciona mirrors para a oficina

--- update-ufpr.sh
+++ update-ufpr.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 jekyll build --config=_config-ufpr.yml
+mkdir -p _site/mirrors
+cp ~/Dropbox/0_Mirror/* _site/mirrors/
+
+# Criação do html
+cat > _site/mirrors/index.html << EOF
+<html>
+<body>
+<ul>
+EOF
+for file in ~/Dropbox/0_Mirror/*
+do
+  file=$(basename $file)
+  echo "<li> <a href=\"$file\"> $file </a>"
+done >> _site/mirrors/index.html
+cat >> _site/mirrors/index.html << EOF
+</ul>
+</body>
+</html>
+EOF
+
 lftp -c "set ftp:list-options -a;
 open abelsiqueira@people.ufpr.br;
 lcd _site;
